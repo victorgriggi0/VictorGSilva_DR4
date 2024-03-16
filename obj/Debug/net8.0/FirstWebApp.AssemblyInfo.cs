85463259c4f71b93745f372cdb94dedbce969f05
victorgriggi0 <victor.gsilva@al.infnet.edu.br>
2024-03-16 14:59:27 -0300

    refactor: readequa projeto para o enunciado do tp3

--- obj/Debug/net8.0/FirstWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30358ddcb352356a135ca80c0ee5b77d360415a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
